559c5af63d8ba04c418b65ed1d96b2d95e45d54f
PauliCZ44 <stastnyp@seznam.cz>
2020-11-01 09:30:47 +0100

    fixing backend connection

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
